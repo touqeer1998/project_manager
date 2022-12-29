4f2c75a3158b1f7810cebe482945475ce75db812
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2022-12-29 19:25:00 +0500

    very first commit of the project having all necessary files

---
+++ app/src/main/java/com/example/projectmanager/splashscreen/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.projectmanager.splashscreen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectmanager.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
