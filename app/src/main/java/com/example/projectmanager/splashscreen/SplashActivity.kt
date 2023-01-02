467fe1e9d1238108978063c43dfaf52513c188c8
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2023-01-02 20:25:39 +0500

    activity_splash_screen.xml and login Design

--- app/src/main/java/com/example/projectmanager/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/example/projectmanager/splashscreen/SplashActivity.kt
@@ -10,6 +10,5 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
     }
 }
