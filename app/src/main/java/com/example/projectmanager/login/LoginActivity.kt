a89488c74161226b8045883ec0a532d0876b6aa4
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2023-03-11 14:35:26 +0500

    activity_login.xml and splash Screen with splashScreen Styles.xml First Commit after Restarting the project

--- app/src/main/java/com/example/projectmanager/login/LoginActivity.kt
+++ app/src/main/java/com/example/projectmanager/login/LoginActivity.kt
@@ -2,11 +2,13 @@ package com.example.projectmanager.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.projectmanager.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_login)
     }
 }
