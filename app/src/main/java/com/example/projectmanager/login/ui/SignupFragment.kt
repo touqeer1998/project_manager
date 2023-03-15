ec753a65ebfe35ff06e9ac9c2e4a6bad6e523014
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2023-03-15 17:08:37 +0500

    eye on password and designing

--- app/src/main/java/com/example/projectmanager/login/ui/SignupFragment.kt
+++ app/src/main/java/com/example/projectmanager/login/ui/SignupFragment.kt
@@ -5,21 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.projectmanager.R
+import com.example.projectmanager.databinding.FragmentMainBinding
+import com.example.projectmanager.databinding.FragmentSignUpBinding
 
 class SignupFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
-
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        //check validity of entered information by user
+        binding.btnSignup.setOnClickListener {
+            if (isInputValid()) {
+                Toast.makeText(requireContext(), "Going to Home", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Invalid Input", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isInputValid():Boolean {
+        return !(binding.etName.text.isEmpty() || binding.etEmail.text.isEmpty() || binding.etPassword.text.isEmpty())
     }
 }
