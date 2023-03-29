64029a5c132d1fe6da1ea23df106ca0c8c875259
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2023-03-29 22:33:49 +0500

    both login and main activity is deleted new a new basic activity is working with fragment

--- app/src/main/java/com/example/projectmanager/login/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/projectmanager/login/ui/SignUpFragment.kt
@@ -1,38 +1,39 @@
 package com.example.projectmanager.login.ui
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.projectmanager.R
-import com.example.projectmanager.databinding.FragmentMainBinding
-import com.example.projectmanager.databinding.FragmentSignUpBinding
-import com.google.android.material.appbar.MaterialToolbar
+import com.example.projectmanager.databinding.FragmentSignupBinding
 
-class SignupFragment : Fragment() {
-    private var _binding: FragmentSignUpBinding? = null
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SignUpFragment : Fragment() {
 
+    private var _binding: FragmentSignupBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        //check validity of entered information by user
         binding.btnSignup.setOnClickListener {
             if (isInputValid()) {
                 Toast.makeText(requireContext(), "Going to Home", Toast.LENGTH_SHORT).show()
@@ -41,13 +42,13 @@ class SignupFragment : Fragment() {
             }
         }
         binding.taSingInInstead.setOnClickListener {
-            findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+            findNavController().navigate(R.id.action_SignUpFragment_to_loginFragment2)
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        requireActivity().actionBar?.title = getString(R.string.sign_up)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun isInputValid():Boolean {
