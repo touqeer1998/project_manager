7abe9ecf1b654a669a2b249b38b491db8d49831d
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2023-03-31 00:28:16 +0500

    initial setup of firebase, setting up few important functions in MainActivity.kt so that can be accessed throughout the app to use anywhere

--- app/src/main/java/com/example/projectmanager/login/ui/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/login/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.projectmanager.login.ui
 
+import android.app.Dialog
+import android.graphics.Color.red
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
@@ -10,11 +15,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.projectmanager.R
 import com.example.projectmanager.databinding.ActivityMainBinding
+import com.example.projectmanager.databinding.ProgressDialogBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var progressDialogBinding: ProgressDialogBinding
+    private lateinit var progressDialog:Dialog
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +34,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        progressDialogBinding = ProgressDialogBinding.inflate(layoutInflater, binding.root, false)
+
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    fun getCurrentUserId(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit(){
+        if (doubleBackToExitPressedOnce){
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Back again to exit",
+            Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({doubleBackToExitPressedOnce = false}, 2000)
+    }
+
+    fun showErrorMessageSnackBar(text: String){
+        val snackBar = Snackbar.make(findViewById(android.R.id.content),
+            text, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(ContextCompat.getColor
+            (this, R.color.error_color))
+        snackBar.show()
+    }
+
+    fun showProgressDialog(text:String){
+        progressDialog = Dialog(this)
+        progressDialog.setContentView(R.layout.progress_dialog)
+
+        progressDialogBinding.tvProgressText.text = text
+
+        progressDialog.show()
+    }
+
+    fun hideProgressDialog(){
+        progressDialog.dismiss()
     }
 
     override fun onSupportNavigateUp(): Boolean {
